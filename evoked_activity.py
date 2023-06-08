5fb8a4f7bb6c60a464499f3642c5dd25c2349abc
Lin <lwang48@mgh.harvard.edu>
2023-06-08 16:00:10 -0400

    check evoked activity to word length

--- evoked_activity.py
+++ evoked_activity.py
@@ -1,20 +1,35 @@
 ## Test evoked activity
-
 import mne
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib
+#matplotlib.use("TkAgg")
 
-my_path = '/Users/linwang/Dropbox (Partners HealthCare)/OngoingProjects/MASC-MEG/'
-epochs_path = my_path + "/bids_anonym"
+my_path = '/Users/linwang/Dropbox (Partners HealthCare)/OngoingProjects/MASC-MEG'
 subject='01'
-epochs = mne.read_epochs(epochs_path)
+epochs_fname = my_path + f"/Segments/sub{subject}"
+epochs = mne.read_epochs(epochs_fname)
 
 ###########################################################
-# Check evoked activity modulated by word length
-epochs.metadata['NumberOfLetters'] = epochs.metadata['word'].apply(lambda x: len(x))
-evokeds = dict()
-query = "NumberOfLetters == {}"
-for n_letters in epochs.metadata["NumberOfLetters"].unique():
-    evokeds[str(n_letters)] = epochs[query.format(n_letters)].average()
-mne.viz.plot_compare_evokeds(evokeds, cmap=("word length", "viridis"), picks="MEG 011")
+# Check evoked activity modulated by word length for content words
+new_epochs = epochs[epochs.metadata['word_category']=='Content']
 
+bin_edges = [1,4,8,17]
+new_epochs.metadata['NumberOfLetters_bin'] = pd.cut(new_epochs.metadata['NumberOfLetters'], bins=bin_edges, labels=False, include_lowest=True) + 1
+new_epochs.metadata['NumberOfLetters_bin'].value_counts()
+evokeds = dict()
+query = "NumberOfLetters_bin == {}"
+picks=['MEG 101','MEG 120']
+for n_letters in new_epochs.metadata["NumberOfLetters_bin"].unique():
+    evokeds[str(n_letters)] = new_epochs[query.format(n_letters)].average().apply_baseline((None, 0))
+mne.viz.plot_compare_evokeds(evokeds, cmap=("word length", "viridis"), picks=picks)
+
+# plot each condition
+evokeds["2"].plot(picks="mag", spatial_colors=True, gfp=True)
+evokeds["2"].plot_joint()
+
+# check sensor layout
+layout = mne.channels.find_layout(new_epochs.info, ch_type='meg')
+layout.plot()
