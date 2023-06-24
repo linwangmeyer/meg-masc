b8045de762e00e60610de3be251c2cdf5f6f5ab1
Lin Wang <lwang11@c1cmp063.pax.tufts.edu>
2023-06-24 16:20:41 -0400

    add .sh script

---
+++ runpython_01.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=jupyter_job
+#SBATCH --time=01-00:20:00  #requested time 1 day and 20 minutes
+#SBATCH -p batch    #running on "batch" partition/queue
+#SBATCH -N 1    #1 nodes
+#SBATCH	-n 2   #2 tasks total
+#SBATCH -c 1	#1 cpu cores per task
+#SBATCH --mem=8g  #requesting 8GB of RAM total
+#SBATCH --output=myjob.%j.out  #saving standard output to file
+#SBATCH --error=myjob.%j.err   #saving standard error to file
+##SBATCH --mail-type=ALL    #email optitions
+##SBATCH --mail-user=your_utln@tufts.edu
+
+#load anaconda module
+module load /cluster/tufts/hpc/tools/anaconda/202111
+
+#activate conda environment, if you use locally installed conda env, make sure use the full path to the env
+conda activate /cluster/tufts/kuperberglab/lwang11/condaenv/mne
+
+python 01_get_epochs_words.py #make sure the results and data generated from your scripts are saved to files
+
+echo "Job completed!"
+
+conda deactivate
+
