7b739510cfc343dadc6e443956ca75fef6bd0c83
Lin <lwang48@mgh.harvard.edu>
2023-07-02 11:37:39 -0400

    get word features

--- 01_generate_words_features.py
+++ 01_generate_words_features.py
@@ -52,20 +52,27 @@ def get_word2vec(metadata):
     return metadata
 
 
-def find_cloze_cw(df_cloze_all,df_words):
-    '''match the cloze values with the cws identified in the MEG data
-    df_cloze_all: cloze values for every word and punctuations in the text data
-    df_words: metadata['words'] associated with the MEG data'''
-    cw_lists = df_words['word'].to_list()
-    for i, word in enumerate(cw_lists):
-        #print(i)
-        #print(word)
-        while df_cloze_all.loc[i, 'words'] != word:
-            pop_ele = df_cloze_all.loc[i, 'words']
-            print(f'pop out {pop_ele}')
-            df_cloze_all = df_cloze_all.drop(i, axis=0)
-            df_cloze_all.reset_index(drop=True, inplace=True)
-    return df_cloze_all
+def find_cw_cloze(df_cloze,df_words):
+    df_words.reset_index(drop=True,inplace=True)
+    rows = []
+    last_index = 0  # Initialize the last index found
+    for index_word, row_word in df_words.iterrows():
+        found = False
+        for index_cloze, row_cloze in df_cloze.loc[last_index:, :].iterrows():
+            if row_cloze['words'].lower() == row_word['word'].lower():
+                rows.append(row_cloze)
+                last_index = index_cloze + 1  # Update the last index found
+                found = True
+                break
+        if not found:
+            print(f"Word '{row_word['word']}' not found in DataFrame.")
+    new_df = pd.DataFrame(rows)
+    new_df.reset_index(drop=True, inplace=True)
+    df = pd.concat([new_df,df_words],axis=1)
+    df = df.drop(['words','story','story_uid','sound_id','sound','onset'],axis=1)
+    df = df.reindex(columns=['word', 'word_index', 'duration', 'probs'])
+    return df
+
 
 
 
@@ -77,7 +84,7 @@ def get_contents_epochs(epochs):
 
 
 ################################################################
-## get cloze value for every word, and match it with epochs
+# get word features for words
 ################################################################
 my_path = r'/Users/linwang/Dropbox (Partners HealthCare)/OngoingProjects/MASC-MEG/'
 #my_path = r'/cluster/home/lwang11/MASC-MEG/'
@@ -86,71 +93,58 @@ session = 0
 task = 0
 epochs_fname = my_path + f"/segments/sub{subject}_session{session}_task{task}.fif"
 epochs = mne.read_epochs(epochs_fname)
-epochs.drop_channels(epochs.info['bads']) #drop bad channels
-epochs.apply_baseline() #baseline correction
+epochs=epochs[epochs.metadata['story']=='lw1']
 
-    
-       
+#get the metadata
+df_words = epochs.metadata
+df_words['word'] = df_words['word'].str.replace('It s', "It's")
 
+# get cloze values
+story = df_words['story'].unique()[0]
+df_fname = my_path + 'stimuli/cloze/story_' + story + '_cloze.csv'
+df_cloze = pd.read_csv(df_fname)
+df_cloze['words'] = df_cloze['words'].astype(str).str.strip()
+df_cw = find_cw_cloze(df_cloze,df_words)
 
-################################################################
-# get other word features for all words
-my_path = r'/Users/linwang/Dropbox (Partners HealthCare)/OngoingProjects/MASC-MEG/'
-#my_path = r'/cluster/home/lwang11/MASC-MEG/'
-subject = str(1).zfill(2)
-session = 0
-task = 0
-word_fname = my_path + f"/segments/words_sub{subject}_session{session}_task{task}.csv"
-df_words = pd.read_csv(word_fname)
-
-# get features
-df_words = get_word_length(df_words)
-df_words = get_wordfreq(df_words)
-df_words = get_syntax(df_words)
+# get other features
+df_cw = get_word_length(df_cw)
+df_cw = get_wordfreq(df_cw)
+df_cw = get_syntax(df_cw)
 #df_words = get_word2vec(df_words)
 
-epochs_fname = my_path + f"/segments/sub{subject}_session{session}_task{task}.fif"
-epochs = mne.read_epochs(epochs_fname)
-story = epochs.metadata['story'].to_list()[0]
-
-df_fname = my_path + 'stimuli/cloze/story_' + story.lower() + '_cloze.csv'
-df_cloze_all = pd.read_csv(df_fname)
-df_cloze_cw = find_cloze_cw(df_cloze_all,df_words)
-
-df_features = df_words.merge(df_cloze_cw, on='word', how='inner')
-df_features = df_features.drop('word', axis=1)
 feature_fname = my_path + f"/segments/features_sub{subject}_session{session}_task{task}.csv"
-df_features.to_csv(feature_fname, index=False)
+df_cw.to_csv(feature_fname, index=False)
 
 
-##########################################
+
+################################################################
 ## merge features with epoch metadata
+################################################################
 feature_fname = my_path + f"/segments/features_sub{subject}_session{session}_task{task}.csv"
-df_features = pd.read_csv(feature_fname)
+df_cw = pd.read_csv(feature_fname)
 
-epochs_fname = my_path + f"/segments/session{session}_sub{subject}_task{task}.fif"
+epochs_fname = my_path + f"/segments/sub{subject}_session{session}_task{task}.fif"
 epochs = mne.read_epochs(epochs_fname)
 epochs.drop_channels(epochs.info['bads']) #drop bad channels
 epochs.apply_baseline() #baseline correction
-metadata = epochs.metadata
-metadata = metadata.merge(df_features, on = 'word', how = 'inner')
-epochs.metadata = metadata
-
+epochs=epochs[epochs.metadata['story']=='lw1']
+epochs.metadata = df_cw
 
 # get only content epochs
 content_epochs = get_contents_epochs(epochs)
-
+metadata = content_epochs.metadata
 metadata['word_freq'].hist()
-
+metadata['probs'].hist()
 
 ################################################################
-# evoked activity to word frequency
-epochs_low = epochs[metadata['word_freq'] < 5]
-epochs_high = epochs[metadata['word_freq'] >= 5]
+# evoked activity to different conditions of features
+################################################################
+epochs_low = content_epochs[(metadata['probs'] < 0.4) & (metadata['probs'] > 0.1)]
+epochs_high = content_epochs[metadata['probs'] >= 0.4]
 
 evokeds = dict()
-evokeds['freq_low'] = epochs_low.average().apply_baseline((None, 0))
-evokeds['freq_high'] = epochs_high.average().apply_baseline((None, 0))
+evokeds['low'] = epochs_low.average().apply_baseline((None, 0))
+evokeds['high'] = epochs_high.average().apply_baseline((None, 0))
 mne.viz.plot_compare_evokeds(evokeds, picks=['MEG 065'])
 
 evoked_diff = mne.combine_evoked([evokeds['freq_low'], evokeds['freq_high']], weights=[1, -1])
@@ -159,11 +153,3 @@ evoked_diff.plot_topomap(times=[0.0, 0.10, 0.40, 0.60], ch_type="mag")
 # check sensor layout
 layout = mne.channels.find_layout(epochs.info, ch_type='meg')
 layout.plot()
-
-################################################################
-# separate stories
-story_names = epochs.metadata['story'].unique().tolist()
-
-# get predictability
-
-
