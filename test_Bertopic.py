92268e94115551efe8dc0f3d5cae35420eb4256e
Lin <lwang48@mgh.harvard.edu>
2023-07-07 16:44:43 -0400

    add bertopic distribution

--- test_Bertopic.py
+++ test_Bertopic.py
@@ -42,3 +42,44 @@ check_df = pd.concat([df['constraint'],result_df],axis=1)
 
 # Show wordcloud
 create_wordcloud(topic_model, topic=999)
+
+topic_distr, _ = topic_model.approximate_distribution(row)
+
+# Similarity of the topic distribution across paragraphs
+R = []
+for index, row in df_sel.iterrows():
+    # Get topic distribution
+    topic_distr, _ = topic_model.approximate_distribution(row)#4parasx2376
+    r = cosine_similarity(topic_distr)
+    indices = np.triu_indices(r.shape[0],k=1)
+    r_vec = r[indices]
+    R.append(r_vec)
+    
+plt.plot(topic_distr[2])
+
+########################################################
+## Concatenate the paragraphs for each item
+df_sel['concatenated'] = df_sel.apply(lambda row: row['gpt4_p1'] + ' ' + row['gpt4_p2'] + ' ' + row['gpt4_p3'] + ' ' + row['gpt4_p4'], axis=1)
+
+# get topic distribution of the concatenated text
+topic_distr, _ = topic_model.approximate_distribution(df_sel['concatenated'])#Nitemx2376
+plt.plot(topic_distr[2])
+
+# calculate entropy
+def calculate_entropy(probabilities):
+    entropy = -np.sum(probabilities * np.log2(probabilities + 1e-12))
+    return entropy
+
+entropies = np.apply_along_axis(calculate_entropy, axis=1, arr=topic_distr)
+df['entropy'] = entropies
+
+# plot the similarity values for each item
+plt.figure(figsize=(10,6))
+plt.bar(df.index, df['entropy'])
+plt.xlabel('condition')
+plt.ylabel('entropy')
+plt.title('topic entropy value by condition')
+plt.xticks(df.index, df['constraint'], rotation=45)
+plt.tight_layout()
+plt.savefig(r'/Users/linwang/Dropbox (Partners HealthCare)/OngoingProjects/LDA/plots/Entropy_4.png')
+plt.show()
--- test_topic_modeling.py
+++ test_topic_modeling.py
@@ -3,60 +3,34 @@ import json
 import mne
 import pandas as pd
 import numpy as np
-from bertopic import BERTopic
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+from gensim.models import LdaModel
+from gensim.corpora import Dictionary
 
-topic_model = BERTopic.load("MaartenGr/BERTopic_Wikipedia")
-#topic_model = BERTopic.load("davanstrien/chat_topics")
-# for a list of pre-trained topics, see: https://huggingface.co/models?library=bertopic&sort=downloads
+# Load the pre-trained LDA model
+pretrained_model = LdaModel.load('path/to/pretrained_model')
 
-def create_wordcloud(model, topic):
-    text = {word: value for word, value in model.get_topic(topic)}
-    wc = WordCloud(background_color="white", max_words=1000)
-    wc.generate_from_frequencies(text)
-    plt.imshow(wc, interpolation="bilinear")
-    plt.axis("off")
-    plt.show()
+# Load your own data
+# Assuming you have a list of documents, where each document is a list of tokens
+your_documents = [
+    ['token1', 'token2', 'token3', ...],
+    ['token4', 'token5', 'token6', ...],
+    ...
+]
 
+# Create a dictionary mapping of word IDs to words
+your_dictionary = Dictionary(your_documents)
 
+# Convert your documents to a Bag-of-Words representation using the dictionary
+your_corpus = [your_dictionary.doc2bow(doc) for doc in your_documents]
 
+# Get the topic distributions for your documents
+topic_distributions = pretrained_model[your_corpus]
 
-## Get data
-fname = r'/Users/linwang/Dropbox (Partners HealthCare)/OngoingProjects/LDA/ALL_EXPERIMENTAL_STIMULI/infocon_stims.csv'
-df = pd.read_csv(fname)
-
-df_topic = pd.DataFrame()
-for column in df.columns:
-    topic_index, prob = topic_model.transform(df[column])
-    column_result = pd.DataFrame({'topic_index': topic_index, 'prob': prob})
-    column_result['condition'] = column
-    df_topic = pd.concat([df_topic, column_result])
-df_topic = df_topic.reset_index(drop=True)
-
-df_topic.groupby('condition').mean()
-df_topic.groupby('condition').std()
-
-
-# Show wordcloud
-create_wordcloud(topic_model, topic=999)
-
-####### Infopos stimuli
-fname = r'/Users/linwang/Dropbox (Partners HealthCare)/OngoingProjects/LDA/ALL_EXPERIMENTAL_STIMULI/InfoTopic_stim_toyData_gpt4_output.csv'
-df = pd.read_csv(fname)
-
-df_sel = df.iloc[:,-8:-1]
-
-# Create empty lists to store the results
-topic_indices = []
-probs = []
-
-# Iterate over each element in the DataFrame
-for index, row in df_sel.iterrows():
-    # Apply topic_model.transform to the element
-    topic_index, prob = topic_model.transform(row[2:])
-    # Append the topic_index and prob values to the corresponding lists
-    topic_indices.append(topic_index)
-    probs.append(prob)
-result_df = pd.DataFrame({'topic_index': topic_indices, 'prob': probs})
-check_df = pd.concat([df_sel['constraint'],result_df],axis=1)
+# Print the topic distributions for each document
+for i, doc_topics in enumerate(topic_distributions):
+    print(f"Document {i+1}:")
+    for topic_id, prob in doc_topics:
+        print(f"Topic {topic_id}: {prob:.4f}")
+    print()
